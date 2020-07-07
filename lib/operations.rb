8410f035ede5c7c33c1bd029a8930e9116d13782
Amanda Walker <walker.akw@gmail.com>
2020-07-07 05:14:28 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,4 +1,4 @@
-require 'pry'
+#require 'pry'
 
 def unsafe?(speed)
 	if speed < 40
@@ -13,5 +13,6 @@ end
 
 
 def not_safe?(speed)
-	binding.pry
+	((speed > 60) || (speed <40)) ? true : false
+	#binding.pry
 end
